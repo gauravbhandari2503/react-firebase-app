a719cd97a49f74088172ffc806dd31bf27f4ed05
gauravb2503 <gaurav.b@simformsolutions.com>
2024-12-03 15:19:22 +0530

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useAuth } from "./../hooks/useAuth";
 
 const Home = () => {
+  const { currentUser } = useAuth();
+  console.log(currentUser);
   return (
     <div className="min-h-screen bg-gray-100 py-8">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold text-center text-blue-600 mb-8">
-            Welcome to React Firebase App
+            Welcome to React Firebase App! <br></br>
+            {currentUser && <span>Welcome {currentUser.email}</span>}
         </h1>
       </div>
     </div>
